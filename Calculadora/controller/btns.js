edccafb2bf94e484da7fb46794c08255064171e9
LeonardoD <LeonardoDoria5@gmail.com>
2025-08-31 16:45:02 -0500

    Controlador de los botones

--- Calculadora/controller/btns.js
+++ Calculadora/controller/btns.js
@@ -1,3 +1,39 @@
 document.getElementById("btn_0").addEventListener("click", function() {
-    this.style.backgroundColor = "red";
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "0";
+});
+
+document.getElementById("btn_1").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "1";
+});
+
+document.getElementById("btn_2").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "2";
+});
+
+document.getElementById("btn_3").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "3";
+});
+
+document.getElementById("btn_4").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "4";
+});
+
+document.getElementById("btn_5").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "5";
+});
+
+document.getElementById("btn_6").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "6";
+});
+
+document.getElementById("btn_7").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "7";
+});
+
+document.getElementById("btn_8").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "8";
+});
+
+document.getElementById("btn_9").addEventListener("click", function() {
+    document.getElementById("Nums").innerHTML = document.getElementById("Nums").innerHTML + "9";
 });
